80d66c23d5904d535020972e9671a62cef7f18cd
Adam Shepherd <ashep15@gmail.com>
2015-07-27 23:37:11 -0400

    pin form website works

---
+++ sites-all/modules/custom/pin_url/theme/pin_url_field.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file pin_url_field.tpl.php
+ */
+
+?><div class="pin-url-markup">
+  <span class="pin-url-markup-title"><?php print $title; ?></span>:
+  <span class="pin-url-markup-value"><?php print $value; ?></span>
+</div>
